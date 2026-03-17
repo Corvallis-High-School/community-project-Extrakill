f5e9b45a4f021cfbc15f747ad9980da626b18833
Extrakill <jason.choi@student.csd509j.net>
2026-03-18 00:31:47 +0900

    Class Library defined

--- LibraryBook.java
+++ LibraryBook.java
@@ -1,6 +1,5 @@
 public class LibraryBook
 {
-    //instance variables
     private int bookID;
     private String title;
     private String genre;
@@ -8,7 +7,16 @@ public class LibraryBook
     private String borrowerName;
     private String dueDate;
 
-    //const
+    public LibraryBook()
+    {
+        bookID = 0;
+        title = "unknown";
+        genre = "unknown";
+        isCheckedOut = false;
+        borrowerName = "none";
+        dueDate = "none";
+    }
+
     public LibraryBook(int id, String title, String genre)
     {
         this.bookID = id;
@@ -19,7 +27,66 @@ public class LibraryBook
         this.dueDate = "none";
     }
 
-    //PM
+    public int getBookID()
+    {
+        return bookID;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public boolean getCheckedOut()
+    {
+        return isCheckedOut;
+    }
+
+    public String getBorrowerName()
+    {
+        return borrowerName;
+    }
+
+    public String getDueDate()
+    {
+        return dueDate;
+    }
+
+    public void setBookID(int id)
+    {
+        bookID = id;
+    }
+
+    public void setTitle(String t)
+    {
+        title = t;
+    }
+
+    public void setGenre(String g)
+    {
+        genre = g;
+    }
+
+    public void setCheckedOut(boolean value)
+    {
+        isCheckedOut = value;
+    }
+
+    public void setBorrowerName(String name)
+    {
+        borrowerName = name;
+    }
+
+    public void setDueDate(String date)
+    {
+        dueDate = date;
+    }
+
     public void print()
     {
         System.out.println("Book ID: " + bookID);
@@ -31,31 +98,6 @@ public class LibraryBook
         System.out.println();
     }
 
-    //GM
-    public int getBookID()      { return bookID; }
-    public String getTitle()    { return title; }
-    public String getGenre()    { return genre; }
-    public String getBorrowerName() { return borrowerName; }
-    public String getDueDate()  { return dueDate; }
-    public boolean getCheckedOut() { return isCheckedOut; }
-
-    //SM
-    public void setBookID(int id)              { bookID = id; }
-    public void setTitle(String t)            { title = t; }
-    public void setGenre(String g)            { genre = g; }
-    public void setBorrowerName(String name)  { borrowerName = name; }
-    public void setDueDate(String date)       { dueDate = date; }
-    public void setCheckedOut(boolean value)  { isCheckedOut = value; }
-
-    //toString
-    public String toString()
-    {
-        return "LibraryBook[ID=" + bookID + ", title=" + title +
-               ", genre=" + genre + ", checkedOut=" + isCheckedOut +
-               ", borrower=" + borrowerName + ", dueDate=" + dueDate + "]";
-    }
-
-    // checkout the book
     public void checkout(String borrower, String date)
     {
         if (!isCheckedOut)
@@ -70,11 +112,20 @@ public class LibraryBook
         }
     }
 
-    // return the book
     public void returnBook()
     {
         isCheckedOut = false;
         borrowerName = "none";
         dueDate = "none";
+    }
+
+    public String toString()
+    {
+        return "LibraryBook[ID=" + bookID
+                + ", title=" + title
+                + ", genre=" + genre
+                + ", checkedOut=" + isCheckedOut
+                + ", borrower=" + borrowerName
+                + ", dueDate=" + dueDate + "]";
     }
 }
